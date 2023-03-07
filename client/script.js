f15bf12286dc5d00a10b271ac8c782c6244f2f09
0xwizz <127023691+0xwizz@users.noreply.github.com>
2023-03-07 13:49:05 +0000

    Update server link

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   const loadingStripe = chatStripe(true, 'Loading...', null, true);
   chatContainer.innerHTML += loadingStripe;
 
-  const response = await fetch('http://localhost:5003', {
+  const response = await fetch('https://booktest5.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
