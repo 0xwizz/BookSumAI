a73a1ad3e5211a21bcee84fef20a4d901731c100
0xwizz <127023691+0xwizz@users.noreply.github.com>
2023-03-08 09:37:28 +0000

    Response time Op

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form');
 const chatContainer = document.getElementById('chat-container');
 
 const generateUniqueId = () => {
-  return Math.random().toString(36).substr(2, 9);
+  return 'id-' + Math.random().toString(36).substr(2, 9);
 };
 
 const chatStripe = (isBot, text, id, isLoading = false) => {
@@ -39,14 +39,14 @@ const typeText = (messageDiv, text) => {
   const lines = text.split('\n').filter(Boolean); // Split text into individual lines
   let currentLine = 0;
 
-  const intervalId = setInterval((lines) => {
+  const intervalId = setInterval(() => {
     if (currentLine < lines.length) {
       const line = lines[currentLine];
       if (line.trim().startsWith("Overview:")) {
-        messageDiv.querySelector('.bot-text').innerHTML += `<br />\n<b>${line}</b><br />\n`; // Add bold tags to Explanation
+        messageDiv.querySelector('.bot-text').innerHTML += `<br />\n<b>${line}</b><br />\n`; // Add bold tags to Overview
         messageDiv.querySelector('.bot-text').innerHTML += '<br />\n'; // Add line break
       } else if (line.trim().startsWith("Key Takeaways:")) {
-        messageDiv.querySelector('.bot-text').innerHTML += `<br />\n<b>${line}</b><br />\n`; // Add bold tags to Decision
+        messageDiv.querySelector('.bot-text').innerHTML += `<br />\n<b>${line}</b><br />\n`; // Add bold tags to Key Takeaways
         messageDiv.querySelector('.bot-text').innerHTML += '<br />\n'; // Add line break
       } else if (line.trim().startsWith("-")) {
         messageDiv.querySelector('.bot-text').innerHTML += `${line}<br />\n`; // Add bullet point
@@ -57,12 +57,8 @@ const typeText = (messageDiv, text) => {
     } else {
       clearInterval(intervalId);
     }
-  }, 50, lines);
-
- 
-
-
-};
+  },);
+}
 
 const handleSubmit = async (e) => {
   e.preventDefault();
@@ -75,37 +71,49 @@ const handleSubmit = async (e) => {
   clearChatContainer();
 
   // Add loading stripe
-  const loadingStripe = chatStripe(true, 'Loading...', null, true);
+  const loadingStripe = chatStripe(true, 'Generating a detailed summary for you...', null, true);
   chatContainer.innerHTML += loadingStripe;
 
-  const response = await fetch('https://booktest5.onrender.com', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      skills: skills,
-      balance: balance,
-    }),
-  });
+  try {
+    const response = await fetch('https://booktest5.onrender.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        skills: skills,
+        balance: balance,
+      }),
+    });
 
-  if (response.ok) {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     const data = await response.json();
     const parsedData = data.bot.trim(); // trims any trailing spaces/'\n'
-  
+
     // Remove loading stripe
     const loadingStripeEl = document.querySelector('.bot-stripe .loader').parentNode.parentNode;
     loadingStripeEl.parentNode.removeChild(loadingStripeEl);
-  
-    // Add bot stripe with response
-    const botStripe = chatStripe(true, '', generateUniqueId(), false); // add an empty text and isLoading=false to prevent the first plain text
-    chatContainer.innerHTML += botStripe;
-    const botStripeEl = document.querySelector(`#${botStripe.match(/id="(.*)"/)[1]}`);
 
-    setTimeout(() => {
-      typeText(botStripeEl, parsedData);
-    }, 90);
-  }
+    // Add bot stripe with response
+const botStripe = chatStripe(true, '', generateUniqueId(), false); // add an empty text and isLoading=false to prevent the first plain text
+chatContainer.innerHTML += botStripe;
+const botStripeEl = document.querySelector(`#${botStripe.match(/id="(.*)"/)[1]}`);
+typeText(botStripeEl, parsedData);
+
+
+} catch (error) {
+  console.error(error);
+  // Remove loading stripe
+  const loadingStripeEl = document.querySelector('.bot-stripe .loader').parentNode.parentNode;
+  loadingStripeEl.parentNode.removeChild(loadingStripeEl);
+
+  // Add error stripe
+const errorStripe = chatStripe(true, 'Sorry, an error occurred. Please try again later.', generateUniqueId(), false);
+chatContainer.innerHTML += errorStripe;
+}
 };
 
 form.addEventListener('submit', handleSubmit);
--- server/server.js
+++ server/server.js
@@ -30,10 +30,10 @@ app.post('/', async (req, res) => {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
-        {"role": "system", "content": "As an expert experienced sage, I can produce incredible detailed summaries of books, allowing users to speed consume information."},
+        {"role": "system", "content": "As an expert experienced sage, I can produce incredible detailed summaries of books, allowing users to speed consume information. People use me to supplement reading fuul books"},
         {"role": "user", "content": `Please write a very detailed informative sumarry of the following book. This should supplement reading the whole book. The title is ${skills} and the author is ${balance}. Include a detailed main explaination titled "Overview" and bullet pointed info and teachings found within, titled "Key Takeaways". Do not include any other text outside these parameters, do not include title and author above`},
       ],
-      max_tokens: 3800,
+  
       
     });
     
